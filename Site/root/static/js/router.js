fbf1e364de3819805cfdd3631281b2363565ef6d
Dan Woodson <woodson.dan@gmail.com>
2012-08-12 15:42:46 -0500

    Homepage slider with Samuel L. Jackson content

--- Site/root/static/js/router.js
+++ Site/root/static/js/router.js
@@ -24,7 +24,7 @@ var page_change = function(innerContent, callback) {
 
 /* Create homepage slide */
 var create_slide = function(img_src, link_url, title, content){
-  
+ console.log(content); 
   new_slide = $('<a class="slider-image" href="'+link_url+'" />');
   new_slide.append('<img class="slider-image" src="'+img_src+'" />');
   new_slide.append('<span><b style="color:white;">'+title+'</b></span>');
@@ -48,8 +48,8 @@ function set_page_background(){
 var home_controller = function(){
   var new_content = $('<div id="sliderHolder" />').append('<div id="features" />')
 
-  new_content.find('#features').append(create_slide('static/images/feature/campbiscoheader.jpg', 'static/images/feature/campbiscoheader.jpg', 'Zoogma to play this years camp bisco', 'Wow-ee!'));
-  new_content.find('#features').append(create_slide('static/images/feature/campbiscoheader.jpg', 'static/images/feature/campbiscoheader.jpg', 'Zoogma to play NEXT years camp bisco', 'Wow-oo!'));
+  new_content.find('#features').append(create_slide('static/images/feature/campbiscoheader.jpg', 'static/images/feature/campbiscoheader.jpg', 'Zoogma to play this years camp bisco', "<h2>Zoogma to play this year's Camp Bisco</h2><div><!-- start slipsum code --><p style='display:block;'>Your bones don't break, mine do. That's clear. Your cells react to bacteria and viruses differently than mine. You don't get sick, I do. That's also clear. But for some reason, you and I react the exact same way to water. We swallow it too fast, we choke. We get some in our lungs, we drown. However unreal it may seem, we are connected, you and I. We're on the same curve, just on opposite ends. </p><p style='display:block;'>You think water moves fast? You should see ice. It moves like it has a mind. Like it knows it killed the world once and got a taste for murder. After the avalanche, it took us a week to climb out. Now, I don't know exactly when we turned on each other, but I know that seven of us survived the slide... and only five made it out. Now we took an oath, that I'm breaking now. We said we'd say it was the snow that killed the other two, but it wasn't. Nature is lethal but it doesn't hold a candle to man. </p><!-- please do not remove this line --><div style='display:none;'><a href='http://slipsum.com'>lorem ipsum</a></div><!-- end slipsum code --></div>"));
+  new_content.find('#features').append(create_slide('static/images/feature/campbiscoheader.jpg', 'static/images/feature/campbiscoheader.jpg', 'Zoogma to play NEXT years camp bisco', "<h2>Zoogma to play next year's Camp Bisco</h2><div><!-- start slipsum code --><p style='display:block;'>Now that there is the Tec-9, a crappy spray gun from South Miami. This gun is advertised as the most popular gun in American crime. Do you believe that shit? It actually says that in the little book that comes with it: the most popular gun in American crime. Like they're actually proud of that shit.</p><p style='display:block;'>Normally, both your asses would be dead as fucking fried chicken, but you happen to pull this shit while I'm in a transitional period so I don't wanna kill you, I wanna help you. But I can't give you this case, it don't belong to me. Besides, I've already been through too much shit this morning over this case to hand it over to your dumb ass.</p><!-- please do not remove this line --><div style='display:none;'><a href='http://slipsum.com'>lorem ipsum</a></div><!-- end slipsum code --></div>"));
 
   page_change(new_content, function(){
     $('#features').coinslider({ 
